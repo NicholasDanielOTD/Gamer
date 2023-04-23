0cc2e0fbd8f67c4a5c662cb9e77f805f6ed18d99
NicholasDanielOTD <nicholasdaniel97@hotmail.com>
2023-04-23 14:44:28 -0500

    some code improvements

--- MyGame/src/Testing/Levels.cs
+++ MyGame/src/Testing/Levels.cs
@@ -5,24 +5,24 @@ namespace MyGame.Testing {
         public static World InitializeMainDebug()
         {
         World world = new World();
+        world.xbound = 16;
+        world.ybound = 16;
         world.GenerateTileArray();
 
         world.entities = new Entity[16];
 
         Entity ball = new Entity();
-        ball.tile = world.TileArray[2,5];
+        ball.SetTile(world.TileArray[2,5]);
         ball.speed = 100f;
         ball.objectKey = "ball";
-        ball.pos = ball.tile.pos;
-        ball.tile.entityOccupier = ball;
+        ball.pos = ball.GetTile().pos;
         world.entities[0] = ball;
 
         Entity ball2 = new Entity();
-        ball2.tile = world.TileArray[3,3];
+        ball2.SetTile(world.TileArray[3,3]);
         ball2.speed = 100f;
         ball2.objectKey = "ball";
-        ball2.pos = ball2.tile.pos;
-        ball2.tile.entityOccupier = ball2;
+        ball2.pos = ball2.GetTile().pos;
         world.entities[1] = ball2;
 
         return world;
