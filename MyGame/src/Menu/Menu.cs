3479e64ca43e3a3dfd64c0034fe185ffe4cf6192
NicholasDanielOTD <nicholasdaniel97@hotmail.com>
2023-04-30 15:49:09 -0500

    fix a bug where tiles weren't updating

--- MyGame/src/Menu/Menu.cs
+++ MyGame/src/Menu/Menu.cs
@@ -1,5 +1,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace MyGame.Menu;
 
@@ -7,11 +9,16 @@ public class GameMenu {
 
     private bool isOpen = false;
     public Texture2D texture;
+    private List<Option> options;
     public GameMenu()
     {
 
     }
 
+    public void AddOption(Option[] options)
+    {
+        this.options.AddRange(options);
+    }
     public void ToggleOpen()
     {
         isOpen = !isOpen;
@@ -29,6 +36,33 @@ public class GameMenu {
         for (int i=0; i < data.Length; i++) data[i] = Color.Black;
         this.texture.SetData(data);
         _spriteBatch.Draw(this.texture, new Vector2(0, 0), Color.Black * .5f);
+
+        foreach(Option opt in options)
+        {
+
+        }
+    }
+
+}
+
+public class Option {
+    
+    private Action _onClick;
+    private Texture2D texture;
+    
+    public Option(Action onClick)
+    {
+        _onClick = onClick;
+    }
+
+    public void SetClick(Action onClick)
+    {
+        _onClick = onClick;
+    }
+
+    public void Draw(SpriteBatch _spriteBatch, Vector2 loc)
+    {
+        _spriteBatch.Draw(this.texture, loc, Color.Black * .5f);
     }
 
 }
